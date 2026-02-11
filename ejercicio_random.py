0f07b84ca63e5982d93923b6a548b4d6b7193149
alba <anoguerag01@educarex.es>
2026-02-11 10:41:54 +0000

    ejercicio random y if else

--- ejercicio_random.py
+++ ejercicio_random.py
@@ -13,5 +13,25 @@ defensa_caballero=random.randint(1,6)
 
 #creacion de la posibilidad de ataque/defensa de cada uno
 if personaje== "a":
+    print("El enano ataca primero !")
     time.sleep(2)
-    print("El enano ataca primero")
+    if ataque_enano > defensa_caballero:
+        print("El enano gana !")
+        print(ataque_enano)
+        print(defensa_caballero)
+    else:
+        print("El caballero gana !")
+        print(ataque_enano)
+        print(defensa_caballero)
+
+if personaje== "b":
+    print("El caballero ataca primero !")
+    time.sleep(2)
+    if ataque_caballero > defensa_enano:
+        print("El caballero gana !")
+        print(ataque_caballero)
+        print(defensa_enano)
+    else:
+        print("El enano gana !")
+        print(ataque_caballero)
+        print(defensa_enano)
