5beedd05d83a2d1ab67c02308009f4dfa646be99
alba <anoguerag01@educarex.es>
2026-02-11 09:53:28 +0000

    ejercicio main

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ nombre= input("escribe tu nombre")
 apellido =input("escribe tu apellido")
 año_nacimiento =input("escribe tu año de nacimiento")
 
-print("tu eres :"+nombre+apellido+año_nacimiento)
+print("tu eres :"+nombre+" "+apellido+" "+año_nacimiento)
