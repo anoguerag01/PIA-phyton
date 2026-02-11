168e733eb7da8567e77dbf88427a0ae4e87b77fb
alba <anoguerag01@educarex.es>
2026-02-11 09:52:40 +0000

    primer ejercicio main

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print("Hola mundo")
+# creacion de variables 
+nombre= input("escribe tu nombre")
+apellido =input("escribe tu apellido")
+año_nacimiento =input("escribe tu año de nacimiento")
+
+print("tu eres :"+nombre+apellido+año_nacimiento)
