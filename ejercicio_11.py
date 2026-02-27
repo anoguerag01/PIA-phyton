84a5f13a41a4933ad7b25dd819619177e68bfa6a
alba <anoguerag01@educarex.es>
2026-02-27 10:25:11 +0000

    ejercicio_11.py

--- ejercicio_11.py
+++ ejercicio_11.py
@@ -5,5 +5,10 @@
 #mostrar por pantalla la cantidad de ahorros tras el primer, segundo y tercer años. 
 #Redondear cada cantidad a dos decimales.
 cuenta_ahorros= float(input("Introduce la cantidad inicial invertda"))
-interes=0.04
-balance_1=cuenta_ahorros*(1+)
+intereses=0.04
+balance_1=cuenta_ahorros*(1+intereses)
+print("El balance del primer año sería: "+ str(round(balance_1,2)))
+balance_2=balance_1*(1+intereses)
+print("El balace del segundo año sería:"+ str(round(balance_2,2))) 
+balance_3=balance_2*(1+intereses)
+print("El balance del tercer año sería: "+str(round(balance_3,2)))
